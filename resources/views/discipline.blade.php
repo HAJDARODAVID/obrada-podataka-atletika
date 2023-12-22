6b235335e9b34478a6ebd30c869b6c78e7f5c9fc
David Hajdarovic <david.hajdarovic@boxmark.com>
2023-12-22 13:55:52 +0100

    added category

--- resources/views/discipline.blade.php
+++ resources/views/discipline.blade.php
@@ -213,6 +213,55 @@
                           </div>
                         </div>
                         </section>
+
+
+                        <section class="py-0 bg-blueGray-50">
+                            <div class="w-full xl:w-8/12 mb-5 xl:mb-0 px-4 mx-auto mt-4">
+                              <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+                                <div class="rounded-t mb-0 px-4 py-3 border-0">
+                                  <div class="flex flex-wrap items-center">
+                                    <div class="relative w-full px-4 max-w-full flex-grow flex-1">
+                                      <h3 class="font-semibold text-base text-blueGray-700">Dostupne kategorije</h3>
+                                    </div>
+                                  </div>
+                                </div>
+                            
+                                <div class="block w-full overflow-x-auto">
+                                  <table class="items-center bg-transparent w-full border-collapse ">
+                                    <thead>
+                                      <tr>
+                                        <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                                      M
+                                                    </th>
+                                      <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                                      Ž
+                                                    </th>
+                                       <th class="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                                                      MIX
+                                                    </th>
+                                      </tr>
+                                    </thead>
+                            
+                                    <tbody>
+                                      <tr>
+                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                                            <input type="checkbox" name="" id="">
+                                        </td>
+                                        <td class="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                            <input type="checkbox" name="" id="">
+                                        </td>
+                                        <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                                          <input type="checkbox" name="" id="">
+                                        </td>
+                                      </tr>
+                                      
+                                    </tbody>
+                            
+                                  </table>
+                                </div>
+                              </div>
+                            </div>
+                            </section>
                 </div>
             </div>
         </div>
